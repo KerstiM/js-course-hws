674436a0cd5989e6089eb0b6a81533fd3d388adc
Kersti Miller <kmiller@itcollege.ee>
2020-03-19 23:40:59 +0200

    refacored utils to be under src

--- HW1/src/index.js
+++ HW1/src/index.js
@@ -1,4 +1,4 @@
-import { Kono } from '../utils/kono';
+import { Kono } from './utils/kono';
 import { PLAYER_1,
          PLAYER_2,
          EMPTY_CELL,
@@ -36,69 +36,63 @@ for (let i = 0; i < NUMBER_OF_ROWS_COLS; i++) {
 
 		if (playerOneBtns(i, j)) {
 		    setAttributes(button, {
-                "class": `grid-btn-item ${PLAYER_1}`,
-                "type": "button",
-                "data-x": i,
-                "data-y": j,
-                "data-value": ""
+                'class': `grid-btn-item ${PLAYER_1}`,
+                'type': "button",
+                'data-x': i,
+                'data-y': j,
+                'data-value': j + i * 5
             });
 		} else if (playerTwoBtns(i, j)) {
             setAttributes(button, {
-                "class":`grid-btn-item ${PLAYER_2}`,
-                "type": "button",
-                "data-x": i,
-                "data-y": j,
-                "data-value": ""
+                'class':`grid-btn-item ${PLAYER_2}`,
+                'type': "button",
+                'data-x': i,
+                'data-y': j,
+                'data-value': j + i * 5
             });
 		} else {
 			setAttributes(button, {
-                "class": `grid-btn-item ${EMPTY_CELL}`,
-                "type": "button",
-                "data-x": i,
-                "data-y": j,
-                "data-value": ""
+                'class': `grid-btn-item ${EMPTY_CELL}`,
+                'type': "button",
+                'data-x': i,
+                'data-y': j,
+                'data-value': j + i * 5
             });
 		}
 		document.getElementById(`${KONO_BOARD}`).appendChild(button);
 	};
 };
 
-for (let i = 0; i < 25; i++) {
-    let button = document.getElementById(`${KONO_BOARD}`);
-    setAttributes(button, {"data-value": i});
-    //document.getElementsByClassName("grid-btn-item").setAttribute("data-value", i);
-}
 // TODO: can this be replaced with kono_board?
 let buttons = document.querySelector('#buttons');
 let gameButtons = document.querySelector(`#${KONO_BOARD}`);
 let allButtons = buttons.querySelectorAll(`.${ALL_BUTTONS}`);
-let emptyCells = buttons.querySelectorAll(`.${EMPTY_CELL}`);
 let display = buttons.querySelector(`.${DISPLAY}`);
 let isPlayerOne = true;
 let clickSum = 0;
+let selectedCoordinates = [];
 
 // listen clicks
 for (const btn of allButtons) {
-    console.log("clicked");
+    console.log('clicked');
+    // players first click can be only on player button not empty button
+    // player second click can only be on empty button
+    // if clicked on disabled button, then click won't count, waiting for a click on a available button
     btn.onclick = getPressedButtonData;
 }
 
 display.innerHTML = displayPlayersOrder(clickSum, isPlayerOne);
 
 let startingPlayer = getCurrentPlayer();
-console.log("startingPlayer", startingPlayer);
+console.log('startingPlayer', startingPlayer);
 getCurrentPlayerButtons(startingPlayer);
 
 function countClicks() {
-  // count clicks only when available click or currentPlayer
-// `"button.${getCurrentPlayer()}.js-disabled"`
-//   if (!buttons.querySelectorAll(".js-disabled")) {
-//       console.log("getCurrentPlayer.getAttribute('class')", getCurrentPlayer.getAttribute('js-disabled'));
-//     }
-  clickSum += 1;
-  console.log("Clicks: ", clickSum);
-  clickSum % 2 === 0 ? toggleCurrentPlayer() : null;
-  display.innerHTML = displayPlayersOrder(clickSum, isPlayerOne);
+    // TODO: count clicks only when available click or currentPlayer
+    clickSum += 1;
+    console.log('Clicks: ', clickSum);
+    clickSum % 2 === 0 ? toggleCurrentPlayer() : null;
+    display.innerHTML = displayPlayersOrder(clickSum, isPlayerOne);
 }
 
 function displayPlayersOrder(clickSum, isPlayerOne) {
@@ -114,7 +108,11 @@ function displayPlayersOrder(clickSum, isPlayerOne) {
 }
 
 function getEmptyCells() {
-    return emptyCells;
+    return buttons.querySelectorAll(`.${EMPTY_CELL}`);
+}
+
+function getAllButtons() {
+    return buttons.querySelectorAll(`.${ALL_BUTTONS}`);
 }
 
 function getCurrentPlayerButtons(currentPlayer) {
@@ -126,34 +124,36 @@ function getCurrentPlayerButtons(currentPlayer) {
 function getPressedButtonData(event) {
     let x = +(this.dataset.x);
     let y = +(this.dataset.y);
+    selectedCoordinates = [x, y];
     let index = y + x * NUMBER_OF_ROWS_COLS;
     let selectedButton = allButtons[index];
     gameButtons.onclick = countClicks;
-
     getAvailableMoves(x, y);
     moveButton(selectedButton);
   }
 
 function getAvailableMoves(x, y) {
     let emptyCells = getEmptyCells();
-
-    emptyCells.forEach(cell => {
-        Math.abs(+cell.dataset.x - x) === 1 && Math.abs(+cell.dataset.y - y) === 1
-        ? addClass(cell, "js-available")
-        : addClass(cell, "js-disabled");
-    });
-    // find if there are cells where x is x+-1 and y is y+-1
-    /*const conditions = [];
     let availableMoves = [];
 
-    return availableMoves;*/
+    emptyCells.forEach(cell => {
+        if (Math.abs(+cell.dataset.x - x) === 1 && Math.abs(+cell.dataset.y - y) === 1) {
+            addClass(cell, 'js-available')
+            availableMoves.push({ y: +cell.dataset.y, x: +cell.dataset.x});
+        } else {
+            addClass(cell, 'js-disabled');
+        }
+    });
+
+    return availableMoves;
 }
 
 function clearAvailableMoves() {
-    let emptyCells = getEmptyCells();
-    emptyCells.forEach(cell => {
-        removeClass(cell, "js-available")
-        removeClass(cell, "js-disabled");
+    let allButtons = getAllButtons();
+
+    allButtons.forEach(cell => {
+        removeClass(cell, 'js-available')
+        removeClass(cell, 'js-disabled');
     });
 }
 
@@ -215,9 +215,15 @@ function getCurrentPlayer() {
     return isPlayerOne ? PLAYER_1 : PLAYER_2;
 }
 
+let togglePlayer = document.getElementById("togglePlayer");
+togglePlayer.addEventListener('click', function() { toggleCurrentPlayer(); }, false);
+
 function toggleCurrentPlayer() {
+    console.log("toggel");
     isPlayerOne = !isPlayerOne;
+    console.log(isPlayerOne);
     displayPlayersOrder(clickSum, isPlayerOne);
+    console.log( displayPlayersOrder(clickSum, isPlayerOne));
     clearAvailableMoves();
 }
 
@@ -229,6 +235,49 @@ function addClass(selectedButton, addedClass) {
     selectedButton.classList.add(addedClass);
 }
 
-function isContaining(selectedButton, containsClass) {
-    selectedButton.classList.contains(containsClass);
+function isGameOver() {
+    // Let's assume that yellow started from top and purple from bottom
+    // find if player 1 buttons are on the bottom
+    // find if player 2 buttons are on the top
+    // display the winner on display
 }
+
+function isClickOnAvailableBtn() {
+    /*
+    // compare if the clicked button is one of the avialble buttons
+    let currentPlayer = getCurrentPlayer(); // player string
+    getCurrentPlayerButtons(currentPlayer);
+    availableMoves = getAvailableMoves(selectedCoordinates[0], selectedCoordinates[1]);
+    console.log('prev', prevAvailableMoves);
+    // selectedCoordinates [0, 2] x:y
+    // availableMoves {y:1, x:1} {y:3, x:1}
+    let res = [];
+    availableMoves.forEach(r => {
+        res.push(Object.values(r));
+        //console.log('Object.values(r)', Object.values(r));
+    });
+    let prevRes = [];
+    prevAvailableMoves.forEach(r => {
+        prevRes.push(Object.values(r));
+    });
+    console.log('prevRes', prevRes);
+    console.log('res', res);
+    console.log('prevSelectedBtnXY', prevSelectedBtnXY);
+
+    // check if the new selected button is one of the previus availablebuttons
+    let result = res.forEach(r => {
+        r.includes(prevSelectedBtnXY[0]) && r.includes(prevSelectedBtnXY[1])
+        console.log("INCLUDES ", r.includes(prevSelectedBtnXY[0]) && r.includes(prevSelectedBtnXY[1]));
+    })
+    prevAvailableMoves = availableMoves;
+    prevSelectedBtnXY.pop();
+    prevSelectedBtnXY.pop();
+    prevSelectedBtnXY.push(selectedCoordinates[0], selectedCoordinates[1]);
+    //console.log('res', res.toString());
+    return result;*/
+}
+/*
+let prevSelectedBtnXY = [];
+let availableMoves = [];
+let prevAvailableMoves = [];
+*/
