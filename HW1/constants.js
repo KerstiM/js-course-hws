d41a470c87bee4c85ff0f7005aee16cb7b5587ab
Kersti Miller <kmiller@itcollege.ee>
2020-03-19 16:06:21 +0200

    Implemented available moves, added constants

--- HW1/constants.js
+++ HW1/constants.js
@@ -1,8 +1,16 @@
 export const PLAYER_1 = 'js-p1';
 export const PLAYER_2 = 'js-p2';
 export const EMPTY_CELL = 'js-empty';
+export const DISPLAY = 'js-display';
+export const KONO_BOARD = 'konoBoard';
+export const ALL_BUTTONS = 'grid-btn-item';
 export const NUMBER_OF_ROWS_COLS = 5;
-
+export const PLAYER_2_ROW_FULL = 4
+export const PLAYER_1_ROW_FULL = 0;
+export const PLAYER_1_ROW_FRAG = 1;
+export const PLAYER_2_ROW_FRAG = 3
+export const PLAYER_LEFT_CELL = 0;
+export const PLAYER_RIGHT_CELL = 4;
 
 export const displayValues = {
     PLAYER_1_STARTS: 'Yellow starts',
--- HW1/src/index.js
+++ HW1/src/index.js
@@ -1,5 +1,18 @@
 import { Kono } from '../utils/kono';
-import { PLAYER_1, PLAYER_2, EMPTY_CELL, NUMBER_OF_ROWS_COLS, displayValues } from '../constants';
+import { PLAYER_1,
+         PLAYER_2,
+         EMPTY_CELL,
+         KONO_BOARD,
+         ALL_BUTTONS,
+         NUMBER_OF_ROWS_COLS,
+         PLAYER_1_ROW_FULL,
+         PLAYER_2_ROW_FULL,
+         PLAYER_1_ROW_FRAG,
+         PLAYER_2_ROW_FRAG,
+         PLAYER_LEFT_CELL,
+         PLAYER_RIGHT_CELL,
+         DISPLAY,
+         displayValues } from '../constants';
 
 let konoBrain = new Kono();
 
@@ -10,43 +23,59 @@ function setAttributes(el, options) {
 };
 
 function playerOneBtns(i, j) {
-	return i === 0 || (i === 1 && (j === 0 || j === 4));
+    return i === PLAYER_1_ROW_FULL || (i === PLAYER_1_ROW_FRAG && (j === PLAYER_LEFT_CELL || j === PLAYER_RIGHT_CELL));
 }
 
 function playerTwoBtns(i, j) {
-	return i === 4 || (i === 3 && (j === 0 || j === 4));
+	return i === PLAYER_2_ROW_FULL || (i === PLAYER_2_ROW_FRAG && (j === PLAYER_LEFT_CELL || j === PLAYER_RIGHT_CELL));
 }
 // generate game board with initial game buttons
-for (let i = 0; i < 5; i++) {
-	for (let j = 0; j < 5; j++) {
+for (let i = 0; i < NUMBER_OF_ROWS_COLS; i++) {
+	for (let j = 0; j < NUMBER_OF_ROWS_COLS; j++) {
 		let button = document.createElement("button");
 
 		if (playerOneBtns(i, j)) {
-		    setAttributes(button, {"class": "grid-btn-item js-p1", "type": "button", "data-x": i, "data-y": j, "data-value": ""});
+		    setAttributes(button, {
+                "class": `grid-btn-item ${PLAYER_1}`,
+                "type": "button",
+                "data-x": i,
+                "data-y": j,
+                "data-value": ""
+            });
 		} else if (playerTwoBtns(i, j)) {
-			setAttributes(button, {"class": "grid-btn-item js-p2", "type": "button", "data-x": i, "data-y": j, "data-value": ""});
+            setAttributes(button, {
+                "class":`grid-btn-item ${PLAYER_2}`,
+                "type": "button",
+                "data-x": i,
+                "data-y": j,
+                "data-value": ""
+            });
 		} else {
-			setAttributes(button, {"class": "grid-btn-item js-empty", "type": "button", "data-x": i, "data-y": j, "data-value": ""});
+			setAttributes(button, {
+                "class": `grid-btn-item ${EMPTY_CELL}`,
+                "type": "button",
+                "data-x": i,
+                "data-y": j,
+                "data-value": ""
+            });
 		}
-		document.getElementById("gameButtons").appendChild(button);
+		document.getElementById(`${KONO_BOARD}`).appendChild(button);
 	};
 };
 
 for (let i = 0; i < 25; i++) {
-    let button = document.getElementById("gameButtons");
+    let button = document.getElementById(`${KONO_BOARD}`);
     setAttributes(button, {"data-value": i});
     //document.getElementsByClassName("grid-btn-item").setAttribute("data-value", i);
 }
-
+// TODO: can this be replaced with kono_board?
 let buttons = document.querySelector('#buttons');
-let gameButtons = document.querySelector('#gameButtons');
-let allButtons = buttons.querySelectorAll('.grid-btn-item');
-let emptyCells = buttons.querySelectorAll('.js-empty');
-let display = buttons.querySelector('.js-display');
-
+let gameButtons = document.querySelector(`#${KONO_BOARD}`);
+let allButtons = buttons.querySelectorAll(`.${ALL_BUTTONS}`);
+let emptyCells = buttons.querySelectorAll(`.${EMPTY_CELL}`);
+let display = buttons.querySelector(`.${DISPLAY}`);
 let isPlayerOne = true;
 let clickSum = 0;
-
 
 // listen clicks
 for (const btn of allButtons) {
@@ -61,6 +90,11 @@ console.log("startingPlayer", startingPlayer);
 getCurrentPlayerButtons(startingPlayer);
 
 function countClicks() {
+  // count clicks only when available click or currentPlayer
+// `"button.${getCurrentPlayer()}.js-disabled"`
+//   if (!buttons.querySelectorAll(".js-disabled")) {
+//       console.log("getCurrentPlayer.getAttribute('class')", getCurrentPlayer.getAttribute('js-disabled'));
+//     }
   clickSum += 1;
   console.log("Clicks: ", clickSum);
   clickSum % 2 === 0 ? toggleCurrentPlayer() : null;
@@ -90,12 +124,38 @@ function getCurrentPlayerButtons(currentPlayer) {
 }
 
 function getPressedButtonData(event) {
-    let value = +(this.dataset.y) + +(this.dataset.x) * NUMBER_OF_ROWS_COLS;
-    let selectedButton = allButtons[value];
+    let x = +(this.dataset.x);
+    let y = +(this.dataset.y);
+    let index = y + x * NUMBER_OF_ROWS_COLS;
+    let selectedButton = allButtons[index];
     gameButtons.onclick = countClicks;
 
+    getAvailableMoves(x, y);
     moveButton(selectedButton);
   }
+
+function getAvailableMoves(x, y) {
+    let emptyCells = getEmptyCells();
+
+    emptyCells.forEach(cell => {
+        Math.abs(+cell.dataset.x - x) === 1 && Math.abs(+cell.dataset.y - y) === 1
+        ? addClass(cell, "js-available")
+        : addClass(cell, "js-disabled");
+    });
+    // find if there are cells where x is x+-1 and y is y+-1
+    /*const conditions = [];
+    let availableMoves = [];
+
+    return availableMoves;*/
+}
+
+function clearAvailableMoves() {
+    let emptyCells = getEmptyCells();
+    emptyCells.forEach(cell => {
+        removeClass(cell, "js-available")
+        removeClass(cell, "js-disabled");
+    });
+}
 
 function getCurrentPlayerButtonsCoordinates(currentPlayer) {
     let currentPlayerBtns = getCurrentPlayerButtons(currentPlayer);
@@ -133,7 +193,7 @@ function moveButton(selectedButton) {
     let currentPlayerBtns = getCurrentPlayerButtonsCoordinates(currentPlayer);
     let selectedBtn = getSelectedButtonCoordinates(selectedButton);
     let emptyCell = getEmptyButtonsCoordinates();
-    // if selected button is current players button then let remove the class
+    // if selected button is current players button then remove the class
     if (isSelectedBtnCurrentPlayerBtn(selectedBtn, currentPlayerBtns)) {
         removeClass(selectedButton, currentPlayer);
         addClass(selectedButton, EMPTY_CELL);
@@ -158,6 +218,7 @@ function getCurrentPlayer() {
 function toggleCurrentPlayer() {
     isPlayerOne = !isPlayerOne;
     displayPlayersOrder(clickSum, isPlayerOne);
+    clearAvailableMoves();
 }
 
 function removeClass(selectedButton, removedClass) {
